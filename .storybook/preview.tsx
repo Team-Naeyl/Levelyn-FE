195b1445a96f6817e4db17f1f5a9461e860402c9
FF <wjdrldud98@gmail.com>
2025-06-12 11:40:17 +0900

    chore: storybook preview에 themeProvider 추가 적용

---
+++ .storybook/preview.tsx
@@ -0,0 +1,35 @@
+import React from 'react';
+import { withThemeFromJSXProvider } from '@storybook/addon-themes';
+import { ThemeProvider } from '@emotion/react';
+
+import { theme } from '../src/styles/theme';
+import GlobalStyles from '../src/styles/GlobalStyles';
+
+import type { Preview } from '@storybook/react-vite';
+
+export const decorators = [
+  withThemeFromJSXProvider({
+    themes: {
+      light: theme,
+    },
+    defaultTheme: 'light',
+    Provider: ThemeProvider,
+    GlobalStyles,
+  }),
+];
+
+const preview: Preview = {
+  parameters: {
+    controls: {
+      matchers: {
+        color: /(background|color)$/i,
+        date: /Date$/i,
+      },
+    },
+    a11y: {
+      test: 'todo',
+    },
+  },
+};
+
+export default preview;
