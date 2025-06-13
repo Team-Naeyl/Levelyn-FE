5b2e8a79a1889ad3e9e3c108d7c52df3f092eee3
FF <wjdrldud98@gmail.com>
2025-06-13 13:48:07 +0900

    refactor: 불필요한 레이아웃 css 제거

--- src/components/layout/BottomNavigation/BottomNavigationLayout.tsx
+++ src/components/layout/BottomNavigation/BottomNavigationLayout.tsx
@@ -4,22 +4,9 @@ import { BottomNavigation } from './_components/BottomNavigation';
 
 export default function BottomNavigationLayout() {
   return (
-    <LayoutWrapper>
-      <ContentWrapper>
-        <Outlet />
-      </ContentWrapper>
+    <>
+      <Outlet />
       <BottomNavigation />
-    </LayoutWrapper>
+    </>
   );
 }
-
-const LayoutWrapper = styled.div`
-  display: flex;
-  flex-direction: column;
-  min-height: 100vh;
-`;
-
-const ContentWrapper = styled.main`
-  flex: 1;
-  padding-bottom: 80px; // BottomNavigation 가리기 방지
-`;
