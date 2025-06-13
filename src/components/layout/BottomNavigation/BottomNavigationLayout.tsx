71f2cff02f3708d920dbe39f68d3f40b9c17d202
FF <wjdrldud98@gmail.com>
2025-06-13 13:58:28 +0900

    fix: 미사용 import 제거

--- src/components/layout/BottomNavigation/BottomNavigationLayout.tsx
+++ src/components/layout/BottomNavigation/BottomNavigationLayout.tsx
@@ -1,5 +1,4 @@
 import { Outlet } from 'react-router-dom';
-import styled from '@emotion/styled';
 import { BottomNavigation } from './_components/BottomNavigation';
 
 export default function BottomNavigationLayout() {
