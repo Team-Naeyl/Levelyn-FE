a0171949d17a88ec6e63ed2a2c67bf9b2183774c
FF <wjdrldud98@gmail.com>
2025-06-18 14:08:42 +0900

    feat: Inventory 페이지 추가 및 라우터 적용

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,8 @@
 import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 import styled from '@emotion/styled';
 import { css } from '@emotion/react';
+import Inventory from './pages/Inventory';
+import BottomNavigationLayout from './components/layout/BottomNavigation/BottomNavigationLayout';
 
 const Header = styled.header`
   ${({ theme }) => css`
@@ -15,6 +17,16 @@ const router = createBrowserRouter([
     element: <Header>Style Test</Header>,
     errorElement: <div>오류!</div>,
     children: [],
+  },
+  {
+    path: 'inventory',
+    element: <BottomNavigationLayout />,
+    children: [
+      {
+        index: true,
+        element: <Inventory />,
+      },
+    ],
   },
 ]);
 
