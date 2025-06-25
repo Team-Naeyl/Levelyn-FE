9183d09ab66187f35de9ceb2718829d8fea83d39
Monixc <chris13404@gmail.com>
2025-06-25 12:42:38 +0900

    feat: API_BASE_URL을 export로 변경
    - API_BASE_URL을 export하여 다른 모듈에서 사용할 수 있도록 수정함.

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API_BASE_URL = 'https://levelyn-api.p-e.kr';
+export const API_BASE_URL = 'https://levelyn-api.p-e.kr';
 
 const api = axios.create({
   baseURL: API_BASE_URL,
