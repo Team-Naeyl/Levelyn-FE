3a30b9b42353936d7315e33fa7f6e298975b194e
Monixc <chris13404@gmail.com>
2025-06-25 13:17:28 +0900

    feat: 인증 제공자 추가 및 렌더링 구조 변경
    - AuthProvider를 추가하여 인증 상태를 관리하도록 수정
    - React.StrictMode로 감싸 렌더링 구조 개선

--- src/main.tsx
+++ src/main.tsx
@@ -1,16 +1,18 @@
-import { StrictMode } from 'react';
-import { createRoot } from 'react-dom/client';
-import { ThemeProvider } from '@emotion/react';
-
+import React from 'react';
+import ReactDOM from 'react-dom/client';
 import App from './App.tsx';
-import GlobalStyles from './styles/GlobalStyles';
+import GlobalStyles from './styles/GlobalStyles.tsx';
 import { theme } from './styles/theme.ts';
+import { ThemeProvider } from '@emotion/react';
+import { AuthProvider } from './contexts/AuthContext.tsx';
 
-createRoot(document.getElementById('root')!).render(
-  <StrictMode>
-    <ThemeProvider theme={theme}>
-      <GlobalStyles />
-      <App />
-    </ThemeProvider>
-  </StrictMode>
+ReactDOM.createRoot(document.getElementById('root')!).render(
+  <React.StrictMode>
+    <AuthProvider>
+      <ThemeProvider theme={theme}>
+        <GlobalStyles />
+        <App />
+      </ThemeProvider>
+    </AuthProvider>
+  </React.StrictMode>
 );
